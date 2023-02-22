4bf4c1460fa786cd1a1918f4aed4b8c815a355fb
Zachary <83608482+Heck-n-Degenerate@users.noreply.github.com>
2023-02-22 01:44:36 -0800

    Update Game.cs

--- Game.cs
+++ Game.cs
@@ -1,5 +1,4 @@
 namespace blackjack;
-
 public class Game
 {
 public Game()
@@ -8,33 +7,66 @@ public Game()
     Deck.Shuffle();
 
     List<Card> PlayerHand = new List<Card>();
-    PlayerHand.Add(new Card(Deck.DrawCard()));
-
+    DrawFor(PlayerHand);
 
     List<Card> DealerHand = new List<Card>();
-    DealerHand.Add(new Card(Deck.DrawCard()));
+    DrawFor(DealerHand);
+
 
     DisplayHand(PlayerHand);
+    Console.WriteLine("Hand value is: " + EvaluateHand(PlayerHand));
+    PromptDraw();
 
-    
-
-    
-}
-void DisplayHand(List<Card> Hand)
+    void PromptDraw()
     {
-    int i = 0;
-    Console.WriteLine("Cards in Hand: ");
-        foreach (Card cards in Hand)
+        Console.Write("Would you like to draw another card? [y/N]: ");
+        string input = Console.ReadLine().ToUpper();
+        switch (input)
         {
-            Console.WriteLine("\t" + Hand[i].name);
-            i++;
+            case "Y": DrawFor(PlayerHand); PromptDraw(); break;
+            default:     
+                DisplayHand(PlayerHand);
+                Console.WriteLine("Hand value is: " + EvaluateHand(PlayerHand));
+            break;
         }
     }
 
+//Internal Methods
+void DrawFor(List<Card> Hand)
+    {
+    Hand.Add(new Card(Deck.DrawCard()));
+    }
+}
+void DisplayHand(List<Card> Hand)
+    {
+    Console.WriteLine("Cards in Hand: ");
+        foreach (Card element in Hand)
+        {
+            Console.WriteLine("\t~" + element.name);
+        }
+    }
 int EvaluateHand(List<Card> Hand)
     {
         int bestValue = 0;
-        return bestValue;
-    }
+        bool hasAce = false;
 
+        foreach (Card element in Hand) {
+            if (element.value == "A") {
+                hasAce = true;
+            }
+        }
+
+        if (hasAce == true)
+        {
+        return -1;
+        }
+        else
+        {
+        foreach (Card element in Hand)
+        {
+            bestValue = bestValue + Convert.ToInt32(element.value);
+        }
+        return bestValue;
+        }
+    }
 }
